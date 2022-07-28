88ca242777abdf0ca6da23dcee60cbbf5e69380a
HRM04 <onigemohameeda@gmail.com>
2022-07-29 00:11:54 +0100

    initial commit

---
+++ Downloads/quiz-game-start_unzip/main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for i in question_data:
+    question_text = i["text"]
+    question_answer = i["answer"]
+    question = Question(question_text, question_answer)
+    question_bank.append(question)
+    print(question_bank)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
